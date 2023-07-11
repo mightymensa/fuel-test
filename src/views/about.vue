7cbc2253107c05b6f0df7d08526aeacfdc56a2da
Ewuradjoa Dadzie <ewura.dadzie@gmail.com>
2023-07-11 21:57:08 +0000

    complete about page

--- src/views/about.vue
+++ src/views/about.vue
@@ -9,15 +9,29 @@
         About
       </div>
 
-      <div class="container"></div>
+      <div class="container">
+        <div>My Fuel Calculator</div>
+        <div class="mt-20">Version : 1.0.0</div>
+        <div class="mt-50">© 2023 Sangie</div>
+      </div>
     </IonContent>
   </ion-page>
 </template>
 
 <script lang="ts" setup>
-import { IonPage, IonButtons, IonCardContent, IonTabBar, IonButton, IonLabel, IonIcon, IonContent } from "@ionic/vue";
-import { useRouter } from "vue-router";
+import { IonPage, IonButton, IonContent } from "@ionic/vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  border-radius: 40px 40px 0 0;
+  position: fixed;
+  text-align: center;
+  padding: 70px;
+  color: rgb(31, 200, 128);
+}
+</style>
