730aadf56d11c309e29ad5c52ac8e878407a1487
mighty <mightymensa40@gmail.com>
2024-04-29 19:23:39 +0000

    refactor

--- src/services/storage.plugin.ts
+++
@@ -1,9 +0,0 @@
-// storagePlugin.ts
-import { App } from "vue";
-import StorageService from "./storage.service";
-
-export default {
-  install(app: App): void {
-    app.provide("StorageService", StorageService);
-  },
-};
