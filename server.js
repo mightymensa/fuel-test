97362699f01da6cfff2b516f47e40aed1100bd82
Ewuradjoa Dadzie <ewura.dadzie@gmail.com>
2024-04-29 19:00:53 +0000

    include server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 80; // Use the provided port by Heroku or default to 3000
+
+// Serve static files from the 'dist' folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Serve the app for all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
